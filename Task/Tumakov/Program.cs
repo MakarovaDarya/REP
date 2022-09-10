677632343646227ebfc35d2c0b9f0fc0c94106dd
Makarova Darya <makarovadarya2704@gmail.com>
2022-09-10 18:55:55 +0300

    first

---
+++ Task/Tumakov/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumakov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 2.1");
+
+        }
+    }
+}
