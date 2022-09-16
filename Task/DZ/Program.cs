f5cf1622d621148c19b5a18bd93c7b9c373daac3
Makarova Darya <makarovadarya2704@gmail.com>
2022-09-16 20:14:17 +0300

    Кл р

--- Task/DZ/Program.cs
+++ Task/DZ/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,20 @@ namespace DZ
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите первый коэффициент");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второй коэффициент");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третий коэффициент");
+            int c = int.Parse(Console.ReadLine());
+            int d = Convert.ToInt32(Math.Sqrt(b * b - 4 * a * c));
+            int x1 = (-b + d) / 2 * a;
+            int x2 = (-b - d) / 2 * a;
+            Console.WriteLine("x1=" + x1);
+            Console.WriteLine("x2=" + x2);
+            Console.ReadKey();
+
+
         }
     }
 }
--- Task/Tumakov/Program.cs
+++ Task/Tumakov/Program.cs
@@ -11,9 +11,62 @@ namespace Tumakov
         static void Main(string[] args)
         {
             Console.WriteLine("Упражнение 2.1");
-            Console.WriteLine("Как вас зовут?");
-            string name =Console.ReadLine();
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
             Console.WriteLine($"Здравствуй, {name}");
+            Console.ReadKey();
+
+            Console.WriteLine("Упражнение 2.2");
+            try
+            {
+                Console.WriteLine("Введите число:");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите число:");
+                int b = int.Parse(Console.ReadLine());
+                int del = a / b;
+                Console.WriteLine($"Результат деления={del}");
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение");
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("ДЗ 2.1");
+            Console.WriteLine("Введите букву латинского алфавита:");
+            string c = Console.ReadLine();
+            if (c == "z")
+            {
+                Console.WriteLine("Следующая буква: а");
+            }
+            else
+            {
+                int code = char.Parse(c);
+                int cod = code + 1;
+                char a = (char)cod;
+                Console.WriteLine($"Следующая буква: " + a);
+
+                Console.WriteLine("ДЗ 2.2");
+            }
+
+            Console.ReadKey();
+            Console.WriteLine("Введите первый коэффициент");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второй коэффициент");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третий коэффициент");
+            int C = int.Parse(Console.ReadLine());
+            int d = Convert.ToInt32(Math.Sqrt(B * B - 4 * A * C));
+            int x1 = (-B + d) / 2 * A;
+            int x2 = (-B - d) / 2 * A;
+            Console.WriteLine("x1=" + x1);
+            Console.WriteLine("x2=" + x2);
+            
+
+
+
+
+
 
 
 
